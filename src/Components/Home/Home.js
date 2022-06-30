1e24755973e4e320e3813c76834976559e97fb36
Hossain-Sajjad <sunshinems58.com>
2022-07-01 01:34:51 +0600

    input field habdled

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HiPlus } from "react-icons/hi";
 
 const Home = () => {
+    const [flag, setFlag] = useState(false);
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            const data = event.target.value;
+            console.log(data)
+            setFlag(false);
+        }
+    }
+    const handleInputField = (event) => {
+        event.preventDefault();
+        setFlag(true);
+    }
+
     return (
         <div>
             <div>
-                <div>
-                    <form action="">
-                        <input type="text" placeholder="Type here" class="input input-bordered input-primary w-full max-w-xs" />
-                    </form>
-                </div>
-                <button class="btn gap-2">
+                {
+                    flag && <div>
+                        <input onKeyDown={handleKeyDown} type="text" placeholder="Type here" class="input input-bordered input-primary w-full max-w-xs" />
+                    </div>
+                }
+                <button onClick={handleInputField} class="btn gap-2">
                     Add a task
                     <HiPlus></HiPlus>
                 </button>
